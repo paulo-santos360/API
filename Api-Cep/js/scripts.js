4e96feedcb1f06f169fb92039b5aecf9c2dde163
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2023-04-07 10:42:56 -0300

    Atualizar

---
+++ Api-Cep/js/scripts.js
@@ -0,0 +1,8 @@
+const addressForm = document.querySelector("#address-form");
+const cepInput = document.querySelector("#cep");
+const addressInput = document.querySelector("#address");
+const cityInput = document.querySelector("#city");
+const neighborhoodInput = document.querySelector("#neighborhood");
+const regionInput = document.querySelector("#region");
+const formInputs = document.querySelectorAll("[data-input]");
+/* Selecionou os input primeiro Todos tem o data-input menos o input Cep*/
