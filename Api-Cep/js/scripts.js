133ecde7299398504ae674b1a5a7446eaf7553f0
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2023-04-07 11:01:05 -0300

    Atualizar

--- Api-Cep/js/scripts.js
+++ Api-Cep/js/scripts.js
@@ -7,4 +7,19 @@ const regionInput = document.querySelector("#region");
 const formInputs = document.querySelectorAll("[data-input]");
 /* Selecionou os input primeiro Todos tem o data-input menos o input Cep*/
 
-const closeButton = document.querySelector("#close-message")
+const closeButton = document.querySelector("#close-message");
+
+//Validação do input cep
+cepInput.addEventListener("keypress", (e) => {
+  const onlyNumbers = /[0-9]|\./;
+  const key = String.fromCharCode(e.keyCode);
+
+  console.log(key);
+  console.log(onlyNumbers.test(key));
+
+  // allow only numbers
+  if (!onlyNumbers.test(key)) {
+    e.preventDefault();
+    return;
+  }
+});
